a2e1567ae4df5bf79664d667173be39932b96765
unknown <jgmejia2@espe.edu.ec>
2022-06-17 00:31:01 -0500

    Code Model Controller Agenda

--- 06-Code/02ServiciosWeb/Codigo de Pepe/api/EndPoints.js
+++ 06-Code/02ServiciosWeb/Codigo de Pepe/api/EndPoints.js
@@ -1,4 +1,5 @@
 const UserController = require('../controller/User') //Importamos el controlador creado
+const EventController = require('../controller/Events')
 const express = require('express') //Importamos el modulo express para la creacion de las uris
 const router = express.Router() //Creamos el router para enrutar las uris con los metodos creados
 
@@ -6,6 +7,12 @@ const router = express.Router() //Creamos el router para enrutar las uris con lo
 //router.metodo_CRUD.('uri', controlador.metodo)
 router.post("/add", UserController.createUser)
 router.get("/all",UserController.getUsers)
+
+router.post()
+router.get()
+router.put()
+router.delete()
+router.put()
 
 module.exports = router //Exportamos el router con las uris creadas
 
---
+++ 06-Code/02ServiciosWeb/Codigo de Pepe/controller/Events.js
@@ -0,0 +1,56 @@
+const Evento = require('../model/event') //importamos el modelo creado
+
+//Metodo para crear un cliente
+const createEvent = (req, res) => { //Tenemos en cuenta los parametros de respuesta (res) y consulta (req)
+    let event = new Evento({ //Creamos un objeto con la informacion desde el body de la consulta
+        Name: req.body.Name,
+        DateEvent: req.body.DateEvent,
+        Affair: req.body.Affair,
+        Content: req.body.Content
+    })
+
+    event.save((err, evt) => { //Guardamos el Cliente obtenido desde el body de la consulta
+        err && res.status(500).json(err.message) //si existe un error, nos mostrara el error
+        res.status(200).json(evt) //Caso contrario nos mostrara el cliente guardado
+    })
+}
+
+//Metodo para crear un cliente
+const getEvent = (req, res) => { //Tenemos en cuenta los parametros de respuesta (res) y consulta (req)
+    Evento.find((err, evt) => { //Obtenemos todos los clientes contenidos en la base de datos
+        err && res.status(500).send(err.message) //si existe un error, nos mostrara el error
+        res.status(200).json(evt) //Caso contrario nos mostrara el arreglo de clientes existentes
+    })
+}
+
+const updateEvent = (req, res) => {
+    Evento.findOneAndUpdate({Name: req.body.Name},{DateEvent: req.body.DateEvent}, (err, evt)=>{
+        err && res.status(500).send(err.message)
+        res.status(200).send(evt)
+    })
+}
+
+const deleteEvent = (req, res) => {
+    Evento.findOneAndDelete({Name: req.body.Name},{DateEvent: req.body.DateEvent}, (err , evt) => {
+        err && res.status(501).send(err.message)
+        res.status(200).send(evt)
+    })
+}
+
+let date = new Date();
+
+const dayEvent = (req, res) => {
+    Evento.findOneAndUpdate({Name: req.body.Name},{DateEvent: req.body.DateEvent}, (err, evt)=>{
+        var fecha1 = req.body.DateEvent;
+        var fecha2 = date.getDate();
+        var dayfecha = fecha2.diff(fecha1, 'days') + ' dias de diferencia';
+        <script src="http://momentjs.com/downloads/moment.min.js"></script>
+        Evento.findOneAndUpdate({Name: req.body.Name},{DateEvent: req.body.DateEvent},{MissingDay: dayfecha},(err, evt)=>{
+            err && res.status(500).send(err.message)
+            res.status(200).send(evt)
+        })
+    })
+}
+
+
+module.exports = {createEvent,getEvent,updateEvent,deleteEvent,dayEvent}
